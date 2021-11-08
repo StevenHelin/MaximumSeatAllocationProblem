fcdd6846a13a60b47c3f913012082802a5730dff
StevenHelin <55350189+StevenHelin@users.noreply.github.com>
2021-11-08 11:55:47 +0100

    Neighborhood matrice d'adjacence

--- src/solvers/neighborhood/Neighborhood.java
+++ src/solvers/neighborhood/Neighborhood.java
@@ -1,21 +1,25 @@
 package solvers.neighborhood;
 
+import data.Amphi;
+
 public class Neighborhood {
-    int[][] matrix;
+    Amphi A= new Amphi();
 
-    public Neighborhood(int[][] matrix) {
-        this.matrix = matrix;
-    }
+    public int[][] buildMatrix(Amphi A){
+        int[][] matrix = new int[0][0];
+        for (int i =0; i<A.getN();i++){
+            for(int j=0; j<A.getN();j++){
+                if(!A.getListSeat().get(i).isFree()){
+                 matrix[i][j]=0;
+                }
+                else{
+                    matrix[i][j]=1;
+                }
+            }
 
-    public int[][] getMatrix() {
+        }
         return matrix;
     }
 
-    public void setMatrix(int[][] matrix) {
-        this.matrix = matrix;
-    }
-
-    public void buildMatrix(int[][] matrix){
-
-    }
 }
+
