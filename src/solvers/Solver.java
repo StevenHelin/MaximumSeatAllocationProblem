bfba8812c0f7f8e494d1b6023634c6f07b4eb4ec
bverhul <55113456+bverhul@users.noreply.github.com>
2021-11-08 08:52:10 +0100

    Update Solver.java
    With comments

--- src/solvers/Solver.java
+++ src/solvers/Solver.java
@@ -1,5 +1,14 @@
 package solvers;
 
+/**
+ * General interface for each solver
+ */
 public interface Solver {
+    /**
+     * Return the execution time of the last procedure call
+     * @return : time in seconds
+     */
     public long executionTime();
+
+    public boolean solve();
 }
