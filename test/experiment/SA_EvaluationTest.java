67d4f18d0f56760ee7c1768127dba3797100eab4
bverhul <55113456+bverhul@users.noreply.github.com>
2022-01-08 10:09:16 +0100

    SA Evaluation Init
    Remove todos on SA

---
+++ test/experiment/SA_EvaluationTest.java
@@ -0,0 +1,43 @@
+package experiment;
+
+import data.Amphi;
+import data.Seat;
+import org.junit.jupiter.api.Test;
+import solvers.HillClimber;
+import solvers.SimulatedAnnealing;
+import solvers.move.PossibleMove;
+import solvers.neighborhood.AroundNeighborhood;
+import solvers.neighborhood.Neighborhood;
+
+import java.io.File;
+import java.util.ArrayList;
+
+public class SA_EvaluationTest {
+    @Test
+    void voisinageTest() {
+        Seat seat = new Seat(true);
+        ArrayList<Seat> listSeats = new ArrayList<>();
+        seat.loadSeat(listSeats);
+        Amphi amphi = new Amphi(listSeats.size(), 100, listSeats);
+
+        SimulatedAnnealing voisin4 = new SimulatedAnnealing(new PossibleMove(),new Neighborhood(amphi,false),100, HillClimber.MoveChoice.DEFAULT);
+
+        Evaluation evaluation = new Evaluation(voisin4,100,amphi.deepCopy());
+        evaluation.experiment();
+        evaluation.exportCSV(new File("sa_experience/voisin4.csv"));
+
+        /* voisinage 8 */
+        SimulatedAnnealing voisin8 = new SimulatedAnnealing(new PossibleMove(),new Neighborhood(amphi,true),100, HillClimber.MoveChoice.DEFAULT);
+
+        evaluation = new Evaluation(voisin8,100,amphi.deepCopy());
+        evaluation.experiment();
+        evaluation.exportCSV(new File("sa_experience/voisin8.csv"));
+
+        /* voisinage par distance */
+        SimulatedAnnealing distance = new SimulatedAnnealing(new PossibleMove(),new AroundNeighborhood(100),100, HillClimber.MoveChoice.DEFAULT);
+
+        evaluation = new Evaluation(distance,100,amphi.deepCopy());
+        evaluation.experiment();
+        evaluation.exportCSV(new File("sa_experience/distance.csv"));
+    }
+}
